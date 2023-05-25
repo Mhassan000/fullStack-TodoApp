4053ce47b5da5e01a68ebea477e8240e9839bd9a
mhassan000 <hassanasif946@gmail.com>
2023-05-25 19:42:34 +0500

    Updated todos routes

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -5,8 +5,8 @@ export async function GET(request: NextRequest) {
     const client  =  await db.connect();
     try {
         await client.sql `CREATE TABLE IF NOT EXISTS Todos (id serial, task varchar(255));`;
-        const tasks = ['Learn Drizzle', 'Learn Next.js'];
-        await client.sql `INSERT INTO Todos (task) VALUES (${tasks[0]}), (${tasks[1]});`;
+        const tasks  = ['learn Postgres', 'learn DrizzleORM']
+        await client.sql `INSERT INTO Todos (task) VALUES (${tasks[0]}), (${tasks[1]}) `
     }catch(error){
         return NextResponse.json({message:'Something went wrong'})
     }
@@ -15,19 +15,3 @@ export async function GET(request: NextRequest) {
 }
 
 
-// export async function GET(request: NextRequest) {
-//     const client = await db.connect();
- 
-//   try {
-//     await client.sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255) );`;
-//     const names = ['Fiona', 'Lucy'];
-//     await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
-//   } catch (error) {
-//     return NextResponse.json({ error }, {
-//         status: 500,
-//       });
-//   }
-
-//   const pets = await client.sql`SELECT * FROM Pets;`;
-//   return NextResponse.json({ pets });
-//   }
