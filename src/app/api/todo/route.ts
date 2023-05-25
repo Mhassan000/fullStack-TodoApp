ec2b150963cf1d013b160288057baeb3a3036012
mhassan000 <hassanasif946@gmail.com>
2023-05-25 20:07:06 +0500

    Updated

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -5,9 +5,6 @@ export async function GET(request: NextRequest) {
     const client  =  await db.connect();
     try {
         await client.sql `CREATE TABLE IF NOT EXISTS Todos (id serial, task varchar(255));`;
-        // const tasks  = ['learn Postgres', 'learn DrizzleORM']
-        // await client.sql `INSERT INTO Todos (task) VALUES (${tasks[0]}), (${tasks[1]}) `
-        
     }catch(error){
         return NextResponse.json({message:'Something went wrong'})
     }
@@ -16,3 +13,17 @@ export async function GET(request: NextRequest) {
 }
 
 
+export async function POST(request:NextRequest){
+    const client =  await db.connect();
+    const req = await request.json();
+    console.log(req);
+    try {
+        if (req.task){
+            await client.sql `INSERT INTO Todos (task) VALUES (${req.task})`
+        }else{
+            throw new Error ('Task field is required')
+        }
+    }catch(error){
+        return NextResponse.json({ message:'Something went wrong'})
+    }
+}
