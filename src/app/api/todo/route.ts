f71d6703663210b8b454e956690631915179e25b
mhassan000 <hassanasif946@gmail.com>
2023-05-25 19:19:02 +0500

    Updated

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -3,13 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     const client  =  await db.connect();
-
     try {
-        await client.sql `CREATE TABLE IF NOT EXISTS Todos (id serial, task varchar(255));`
-
-
+        await client.sql `CREATE TABLE IF NOT EXISTS Todos (id serial, task varchar(255));`;
     }catch(error){
-        return NextResponse.json  ({message:'Something went wrong'})
+        return NextResponse.json({message:'Something went wrong'})
     }
 }
 
