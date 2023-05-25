dd2185769961a4d005960a968b1fc600fcf2b668
mhassan000 <hassanasif946@gmail.com>
2023-05-25 19:58:14 +0500

    Updated

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -5,8 +5,9 @@ export async function GET(request: NextRequest) {
     const client  =  await db.connect();
     try {
         await client.sql `CREATE TABLE IF NOT EXISTS Todos (id serial, task varchar(255));`;
-        const tasks  = ['learn Postgres', 'learn DrizzleORM']
-        await client.sql `INSERT INTO Todos (task) VALUES (${tasks[0]}), (${tasks[1]}) `
+        // const tasks  = ['learn Postgres', 'learn DrizzleORM']
+        // await client.sql `INSERT INTO Todos (task) VALUES (${tasks[0]}), (${tasks[1]}) `
+        
     }catch(error){
         return NextResponse.json({message:'Something went wrong'})
     }
