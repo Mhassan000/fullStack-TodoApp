561fc9b485a2b9cb4466c0d46952280d8a191580
mhassan000 <hassanasif946@gmail.com>
2023-05-25 19:12:02 +0500

    created api routes

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,8 +1,16 @@
-// import { db } from '@vercel/postgres';
+import { db } from '@vercel/postgres';
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-    return NextResponse.json({ message: 'hello world' });
+    const client  =  await db.connect();
+
+    try {
+        await client.sql `CREATE TABLE IF NOT EXISTS Todos (id serial, task varchar(255));`
+
+
+    }catch(error){
+        return NextResponse.json ({message:'Something went wrong'})
+    }
 }
 
 
