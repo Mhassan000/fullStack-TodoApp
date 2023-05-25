5549a7c2ee61c04fd5751fd6ec39eb590defe553
mhassan000 <hassanasif946@gmail.com>
2023-05-25 19:25:31 +0500

    Updated

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -11,19 +11,3 @@ export async function GET(request: NextRequest) {
 }
 
 
-// export async function GET(request: NextRequest) {
-//     const client = await db.connect();
- 
-//   try {
-//     await client.sql`CREATE TABLE IF NOT EXISTS Pets ( Name varchar(255), Owner varchar(255) );`;
-//     const names = ['Fiona', 'Lucy'];
-//     await client.sql`INSERT INTO Pets (Name, Owner) VALUES (${names[0]}, ${names[1]});`;
-//   } catch (error) {
-//     return NextResponse.json({ error }, {
-//         status: 500,
-//       });
-//   }
-
-//   const pets = await client.sql`SELECT * FROM Pets;`;
-//   return NextResponse.json({ pets });
-//   }
