384c813639f0a7d4c0774cfe021a35097567e1d6
mhassan000 <hassanasif946@gmail.com>
2023-05-29 16:40:38 +0500

    Implemented drizzle orm

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,31 +1,47 @@
-import { db } from '@vercel/postgres';
 import { NextRequest, NextResponse } from "next/server";
+import { Todo,newTodo,db,todoTable } from '@/app/lib/drizzle';
+import { sql } from "@vercel/postgres";
+
 
 export async function GET(request: NextRequest) {
-    const client  =  await db.connect();
-    try {
-        await client.sql `CREATE TABLE IF NOT EXISTS Todos (id serial, task varchar(255));`;
+    // Get request using drizzle
+
+    try{
+        await sql `CREATE TABLE IF NOT EXISTS Todos (id serial, task varchar(255));`;
+        const res = await db.select().from(todoTable);
+        return NextResponse.json({data: res});
     }catch(error){
-        return NextResponse.json({message:'Something went wrong'})
+        console.log ((error as {message:string}).message);
+        return NextResponse.json ({message:'Something went wrong'})
+
     }
-    const todos = await client.sql `SELECT * FROM Todos;`;   
-    return NextResponse.json({todos});
+
+
+    // Get request using postgres
+    // const client  =  await db.connect(); 
+    // try {
+    //     await sql `CREATE TABLE IF NOT EXISTS Todos (id serial, task varchar(255));`;
+    // }catch(error){
+    //     return NextResponse.json({message:'Something went wrong'})
+    // }
+    // const todos = await client.sql `SELECT * FROM Todos;`;   
+    // return NextResponse.json({todos});
 }
 
 
-export async function POST(request:NextRequest){
-    const client =  await db.connect();
-    const req = await request.json();
-    console.log(req);
-    try {
-        if (req.task){
-            await client.sql `INSERT INTO Todos (task) VALUES (${req.task})`
-            return NextResponse.json({message: 'data added successfully'})
-        }else{
-            throw new Error ('Task field is required')
-        }
-    }catch(error){
-        return NextResponse.json({message: error as {message:string}}.message)
-    }
+// export async function POST(request:NextRequest){
+//     const client =  await db.connect();
+//     const req = await request.json();
+//     console.log(req);
+//     try {
+//         if (req.task){
+//             await client.sql `INSERT INTO Todos (task) VALUES (${req.task})`
+//             return NextResponse.json({message: 'data added successfully'})
+//         }else{
+//             throw new Error ('Task field is required')
+//         }
+//     }catch(error){
+//         return NextResponse.json(( error as {message:string}).message)
+//     }
     
-}
+// }
